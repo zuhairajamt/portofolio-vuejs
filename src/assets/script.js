16709e653ae861a3ddd7fbe460d37b1ca0f337fa
zuhairajamt <zuhairajamt@gmail.com>
2022-10-18 14:36:27 +0700

    pinia contact hore

--- src/assets/script.js
+++ src/assets/script.js
@@ -20,14 +20,14 @@ window.addEventListener('DOMContentLoaded', event => {
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 72,
-        });
-    };
+    // // Activate Bootstrap scrollspy on the main nav element
+    // const mainNav = document.body.querySelector('#mainNav');
+    // if (mainNav) {
+    //     new bootstrap.ScrollSpy(document.body, {
+    //         target: '#mainNav',
+    //         offset: 72,
+    //     });
+    // };
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
@@ -41,4 +41,7 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+
+
+
 });
